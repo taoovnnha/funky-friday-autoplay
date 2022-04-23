abee9f3eb6a0fd9a9f9feceadce73d13557215db
wally <38594850+wally-rblx@users.noreply.github.com>
2022-04-23 03:25:24 -0500

    Update metadata.lua

--- metadata.lua
+++ metadata.lua
@@ -1,5 +1,5 @@
 return {
 	version = '2.1',
-	updated = '4/8/22',
-	message = 'Thank you for using this script and all the support!'
+	updated = '4/23/22',
+	message = 'Updated discord invite :)'
 }
